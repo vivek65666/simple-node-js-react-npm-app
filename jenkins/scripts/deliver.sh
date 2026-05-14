ee0cb23ae5ed4d90ddcb03399ce5a1c2e1449e68
Vivek <vivekcraj321@gmail.com>
2026-05-14 18:16:46 +0530

    Force commit of all files

---
+++ jenkins/scripts/deliver.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+echo 'The following "npm" command builds your Node.js/React application for'
+echo 'production in the local "build" directory using Vite, correctly bundles'
+echo 'React in production mode and optimizes the build for the best performance.'
+set -x
+npm run build
+set +x
+
+echo 'The following "npm" command runs your Node.js/React application in'
+echo 'development mode and makes the application available for web browsing.'
+echo 'The "npm start" command has a trailing ampersand so that the command runs'
+echo 'as a background process (i.e. asynchronously). Otherwise, this command'
+echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
+echo 'is followed by another command that retrieves the process ID (PID) value'
+echo 'of the previously run process (i.e. "npm start") and writes this value to'
+echo 'the file ".pidfile".'
+set -x
+npm start &
+sleep 1
+echo $! > .pidfile
+set +x
+
+echo 'Now...'
+echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
